9fbef4c978d581c2b4188e68c9e49745e2bfef2e
Dorson Tang <apersonnameddorson@gmail.com>
2025-02-17 20:34:22 -0800

    rough positions of thrusters

--- src/triton_gazebo/launch/barebones_teleop_launch.py
+++ src/triton_gazebo/launch/barebones_teleop_launch.py
@@ -10,9 +10,6 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ld = LaunchDescription()
 
-    pkg_share = get_package_share_directory('triton_gazebo')
-    sdf_file =  os.path.join(pkg_share, 'gazebo', 'models', 'triton_auv', 'model.sdf')
-
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('triton_gazebo'), 'launch', 'gazebo_launch.py')
@@ -32,15 +29,8 @@ def generate_launch_description():
         )
     )
 
-    # underwater_camera = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         get_package_share_directory('triton_gazebo') + '/launch/underwater_camera_launch.py'
-    #     )
-    # )
-
     ld.add_action(gazebo)
     ld.add_action(thrust_allocator)
     ld.add_action(keyboard_teleop)
-    # ld.add_action(underwater_camera)
 
     return ld
