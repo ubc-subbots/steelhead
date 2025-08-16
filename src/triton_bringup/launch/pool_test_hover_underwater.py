1049066eea0b5885b6a4dcc96b37e2d40e6324bc
Joel Hempel <joelhempel35@gmail.com>
2025-08-15 23:38:04 -0700

    fixed hover launch

--- src/triton_bringup/launch/pool_test_hover_underwater.py
+++ src/triton_bringup/launch/pool_test_hover_underwater.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
+from launch_ros.actions import Node, ComposableNodeContainer
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.descriptions import ComposableNode
 from ament_index_python.packages import get_package_share_directory
 import os
 
@@ -10,13 +12,53 @@ def generate_launch_description():
     """
     ld = LaunchDescription()
 
-    # Include the hover underwater launch from triton_controls
+    serial = ComposableNodeContainer(
+            name='serial_subscriber_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='triton_controls',
+                    plugin='triton_controls::SerialSubscriber',
+                    name='serial_subscriber'),
+            ],
+            output='both',
+    ) 
+
+    ta_config = os.path.join(
+        get_package_share_directory('triton_controls'),
+        'config',
+        'thruster_config.yaml'
+    )
+
+    thrust_allocator = Node(
+        name='thrust_allocator',
+        namespace='/triton/controls',
+        package='triton_controls',
+        executable='thrust_allocator',
+        output='screen',
+        parameters=[ta_config],
+        remappings=[
+            ('/triton/controls/signals', '/motor_control')
+        ]
+    )
+
     hover_underwater = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('triton_controls'), 'launch', 'hover_underwater_launch.py')
         )
     )
 
-    ld.add_action(hover_underwater)
+    imu = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('triton_bringup'), 'imu_launch.py')
+        )
+    )
 
+    ld.add_action(serial)
+    ld.add_action(thrust_allocator)
+    ld.add_action(hover_underwater)
+    ld.add_action(imu)
+    
     return ld
