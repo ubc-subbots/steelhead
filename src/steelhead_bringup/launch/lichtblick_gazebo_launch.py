fc81802db1ac186d87b173fbef7bceba9576e44d
Krish <garchakri@gmail.com>
2026-04-27 18:39:52 +0000

    new simple launch for lichtblick testing

---
+++ src/steelhead_bringup/launch/lichtblick_gazebo_launch.py
@@ -0,0 +1,33 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('steelhead_gazebo'), 'launch', 'gazebo_launch.py')
+        ),
+        launch_arguments={'world': 'competition.world'}.items()
+    )
+
+    thrust_allocator = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('steelhead_controls'), 'launch', 'thrust_allocator_launch.py')
+        )
+    )
+
+    actuators_simulation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('steelhead_gazebo'), 'launch', 'actuators_simulation_launch.py')
+        )
+    )
+
+    ld.add_action(gazebo)
+    ld.add_action(thrust_allocator)
+    ld.add_action(actuators_simulation)
+
+    return ld
