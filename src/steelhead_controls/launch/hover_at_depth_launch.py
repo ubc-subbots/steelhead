bb648483aaa2047f157aa92d5455b754edc7c0a5
Dorson Tang <apersonnameddorson@gmail.com>
2026-06-02 22:01:30 -0700

    hover script and pid roll, pitch and yaw

---
+++ src/steelhead_controls/launch/hover_at_depth_launch.py
@@ -0,0 +1,22 @@
+
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    hover_at_depth = Node(
+        package='steelhead_controls', 
+        executable='hover_at_depth',
+        parameters=[{'depth': 0.5}],
+        namespace="steelhead"
+    )
+
+    ld.add_action(hover_at_depth)
+
+    return ld
