1f9305a7e3b4824dc990b381fa78a5a27707647b
Dorson Tang <120761661+doruphin@users.noreply.github.com>
2026-06-06 13:40:03 -0700

    Comment out delayed_rqt action in launch file
    Comment out the delayed_rqt action to prevent it from executing.

--- src/steelhead_bringup/launch/gazebo_test_hover_launch.py
+++ src/steelhead_bringup/launch/gazebo_test_hover_launch.py
@@ -62,6 +62,6 @@ def generate_launch_description():
     ld.add_action(keyboard_teleop)
     ld.add_action(hover_script)
     ld.add_action(pid_controller)
-    ld.add_action(delayed_rqt) # let gazebo spin up
+    # ld.add_action(delayed_rqt) # let gazebo spin up
 
     return ld
